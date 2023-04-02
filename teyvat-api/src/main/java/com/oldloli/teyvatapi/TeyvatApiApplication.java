4807d24ba19f502d48418426914c6e52be7e298e
浪浪老萝莉 <44917405+zxcvbnm002400@users.noreply.github.com>
2023-04-02 23:22:38 +0800

    后端创建

---
+++ teyvat-api/src/main/java/com/oldloli/teyvatapi/TeyvatApiApplication.java
@@ -0,0 +1,13 @@
+package com.oldloli.teyvatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeyvatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeyvatApiApplication.class, args);
+    }
+
+}
---
+++ teyvat-api/src/test/java/com/oldloli/teyvatapi/TeyvatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldloli.teyvatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeyvatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
